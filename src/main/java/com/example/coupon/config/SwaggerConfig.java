29e956d5b4127bfa7259209fd0ec76d36bc38676
dave915 <htaehyun@gmail.com>
2020-07-07 02:46:23 +0900

    [refactor] swagger에 인증 header 넣을 수 있도록 수정

--- src/main/java/com/example/coupon/config/SwaggerConfig.java
+++ src/main/java/com/example/coupon/config/SwaggerConfig.java
@@ -2,14 +2,21 @@ package com.example.coupon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Configuration
@@ -22,6 +29,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .genericModelSubstitutes(Optional.class, Mono.class, Flux.class);
+                .genericModelSubstitutes(Optional.class, Mono.class, Flux.class)
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", HttpHeaders.AUTHORIZATION, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = { new AuthorizationScope("global", "accessEverything") };
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 }
